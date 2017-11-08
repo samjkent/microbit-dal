2593790513f231e62e561bf2d27675c89e6cbce8
Sam <sam@microbit.org>
2017-11-08 14:27:13 +0000

    Adjust Flash Pointer

--- source/bluetooth/MicroBitPartialFlashingService.cpp
+++ source/bluetooth/MicroBitPartialFlashingService.cpp
@@ -37,7 +37,7 @@ uint8_t *MicroBitPartialFlashService::data = 0;         // access static var
 uint32_t MicroBitPartialFlashService::offset = 0;
 
 uint32_t *scratchPointer = (uint32_t *)(NRF_FICR->CODEPAGESIZE * (NRF_FICR->CODESIZE - 19));
-uint32_t *flashPointer   = (uint32_t *)0x31000; // (uint32_t *)(FLASH_PROGRAM_END);// (uint32_t *)memoryMap.memoryMapStore.memoryMap[2].startAddress;
+uint32_t *flashPointer   = (uint32_t *)(FLASH_PROGRAM_END); // (uint32_t *)memoryMap.memoryMapStore.memoryMap[2].startAddress;
 
 /**
   * Constructor.
@@ -79,6 +79,11 @@ MicroBitPartialFlashService::MicroBitPartialFlashService(BLEDevice &_ble, MicroB
 
     // Set up listener for SD writing
     messageBus.listen(MICROBIT_ID_PFLASH_NOTIFICATION, MICROBIT_EVT_ANY, writeEvent);
+
+    // Set up flash pointer to magic
+    while(&flashPointer != 0x708E3B92){
+        &flashPointer = &flashPointer + 0x10;
+    } 
 
 }
 
