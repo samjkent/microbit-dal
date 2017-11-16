b7170d31e72510f4915c71527daf33958a621fb8
Sam <sam@microbit.org>
2017-11-16 16:06:42 +0000

    Flash 16 bytes at once

--- source/bluetooth/MicroBitPartialFlashingService.cpp
+++ source/bluetooth/MicroBitPartialFlashingService.cpp
@@ -127,7 +127,7 @@ void MicroBitPartialFlashService::writeEvent(MicroBitEvent e){
     offset = (data[16] << 8) | data[17];
     offset = offset / 4;
 
-    writeStatus = flash.flash_write(flashPointer + offset + i, data, sizeof(data), scratchPointer);
+    writeStatus = flash.flash_write(flashPointer + offset, data, 16, scratchPointer);
 
 }
 
